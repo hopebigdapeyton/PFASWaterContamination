791511629a23e94ec04ed19d09b4c5da4deefda8
hopebigdapeyton <57331739+hopebigdapeyton@users.noreply.github.com>
2019-11-06 22:03:02 -0500

    Add files via upload

---
+++ dataCleaning.R
@@ -0,0 +1,24 @@
+# Data cleaning
+
+
+# setup
+# install.packages("tidyverse")
+
+library(tidyverse)
+library(readr)
+
+# rm(MPART_Original_Unique_Values)
+mydata <- read_csv("mydata_v2.csv", na = c("", "NA"))
+
+names(mydata)[1] <- "COUNTYNAME"
+names(mydata)
+
+names(mydata)[25] <- "TOTALPFAS"
+
+p <- ggplot(data = mydata, mapping = aes(x = TOTALPFAS, y = COUNTYNAME))
+p + geom_point(color = "purple") + geom_smooth(method = "loess") + scale_x_log10() + labs (x = "Parts per Trillion", y = "Michigan Counties", title = "PFAS Concentration Levels in Michigan Counties", subtitle = "data points are calculated total PFAS", caption = "Source: Michigan PFAS Response.")
+
+p <- ggplot(data = mydata, mapping = aes(x = TOTALPFAS, y = COUNTYNAME))
+p + geom_point(color = "purple") + geom_smooth(method = "loess") + scale_x_sqrt() + labs (x = "Parts per Trillion", y = "Michigan Counties", title = "PFAS Concentration Levels in Michigan Counties", subtitle = "data points are calculated total PFAS", caption = "Source: Michigan PFAS Response.")
+
+
